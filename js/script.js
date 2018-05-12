fbc3fcd6a90a34b0d0c5704cba4e105465920f48
Elton Memishaj <eltoni.memishi@live.com>
2018-05-12 20:43:24 +0200

    Added onclick listener for FAB to reset form fields.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
   $('header > nav').css('padding-right', $('header > .logo-and-title').outerWidth() + 'px'); // Centering Header's nav.
+  $('#clear-form-button').on('click', function() { $('#first-form')[0].reset(); });
 });
