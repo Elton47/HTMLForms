4ffa358e1730aef2a6ea7027150755b4dcec2acc
Elton Memishaj <eltoni.memishi@live.com>
2018-05-13 01:54:47 +0200

    Added status-bar button events, input field value listeners.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
-  $('header > nav').css('padding-right', $('header > .logo-and-title').outerWidth() + 'px'); // Centering Header's nav.
   $('#clear-form-button').on('click', function() { $('#first-form')[0].reset(); });
+  $('li[title*="inimize"]').click(function() { minimizeWindow($('#first-form')); }); /* Excluding the first character of "title" attribute, to ignore cases */
+  $('li[title*="esize"]').click(function() { resizeWindow($('#first-form')); });
+  $('li[title*="lose"]').click(function() { closeWindow($('#first-form')); });
+  Array.from($('input')).forEach(element => { /* Input field labels animation based on empty CSS class for input fields */
+    checkField(element);
+    $(element).blur(function() { checkField(element); });
+  });
 });
+function closeWindow(e) {
+  e.hide('fast', function() {
+    e.remove();
+  });
+}
+function resizeWindow(e) {
+  if(e.hasClass('minimized'))
+    e.removeClass('minimized');
+  e.toggleClass('maximized');
+}
+function minimizeWindow(e) {
+  if(e.hasClass('maximized'))
+    e.removeClass('maximized');
+  e.toggleClass('minimized');
+}
+function checkField(e) {
+  var input = $(e);
+  if(input.val() != '')
+    input.removeClass('empty');
+  else
+    input.addClass('empty');
+}
